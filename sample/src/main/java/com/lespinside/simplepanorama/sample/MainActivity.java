881d6ca8cfc2d70da4a3d8cb2e14118e256da76d
walpokes <lespinsideg@gmail.com>
2016-11-13 21:21:27 +0900

    add SphericalView sample.

--- sample/src/main/java/com/lespinside/simplepanorama/sample/MainActivity.java
+++ sample/src/main/java/com/lespinside/simplepanorama/sample/MainActivity.java
@@ -4,10 +4,37 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.lespinside.simplepanorama.view.SphericalView;
+import com.panoramagl.utils.PLUtils;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SphericalView sphericalView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sphericalView = (SphericalView) findViewById(R.id.spherical_view);
+        sphericalView.setPanorama(PLUtils.getBitmap(this, R.raw.sunset_at_pier), false);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sphericalView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sphericalView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        sphericalView.onDestroy();
     }
 }
